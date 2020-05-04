767fc67549fa5bbc67b87c9909c8e764373bbb16
milosjakoubek <64581735+milosjakoubek@users.noreply.github.com>
2020-05-04 11:50:53 +0200

    Add static and format code

--- sketch.js
+++ sketch.js
@@ -1,97 +1,139 @@
-// create a loop for multiple images with .get
-// change the nature of each image
-// make them move independently
-
 let img;
-let xpos1 = 0;
-let xpos2 = 0;
+let xoff1 = 0;
+let xoff2 = 0;
 let xspeed1 = 0;
 let xspeed2 = 0;
-// let shard1 = 100;
-// let shard2 = 200;
 
-function preload(){
-	img = loadImage('assets/david19.jpg');
+// captures millis
+let mstime = 0;
+
+
+function preload() {
+  img = loadImage('assets/david19.jpg');
 }
 
-// break down the image to x chunks
+// global setup function
 function setup() {
+  initSetup();
+}
+
+// global draw function
+function draw() {
+  initDraw();
+}
+
+// local setup function
+function initSetup() {
+  mstime = millis();
   createCanvas(img.width, img.height);
+  // adjust pixelDensity to default
   pixelDensity(1);
   img.filter(GRAY);
-  david1 = img.get(0, 0, img.width, img.height);
-  david2 = img.get(0, 0, img.width, img.height);
-  // shard = img.get(0, shard1, img.width, shard2);
-  
+  // get pixels from original asset
+  reddavid = img.get(0, 0, img.width, img.height);
+  bluedavid = img.get(0, 0, img.width, img.height);
 }
 
-function draw(){
+// local draw function
+function initDraw() {
+  // draw original asset with gray filter
   tint(255, 75)
   image(img, 0, 0);
+  // tint original asset to red and draw [red David]
   tint(255, 0, 0, 75);
-  image(david1, xpos1, 0);
+  image(reddavid, xoff1, 0);
+  // tint original asset to shade of blue and draw [blue David]
   tint(0, 255, 255, 75);
-  image(david2, xpos2, 0);
+  image(bluedavid, xoff2, 0);
   noTint();
-  
-  
-    if(int(frameCount/10)%25 == int(0)) {
-    shard(random(0,img.height - 100), 100);
-    frameRate(8);
-  } else if(int(frameCount/10)%15 == int(0)) {
-    fshard(150, 100);
+
+  // movement of red david
+  if (xoff1 == 0) {
+    xspeed1 = -0.5;
+  }
+  if (xoff1 < -10) {
+    xspeed1 = +0.5;
+  }
+  xoff1 = xoff1 + xspeed1;
+
+  // movement of blue david
+  if (xoff2 > 10) {
+    xspeed2 = -0.5;
+  }
+  if (xoff2 == 0) {
+    xspeed2 = +0.5;
+  }
+  xoff2 = xoff2 + xspeed2;
+
+  // draw shards at slowed framerate with pause in between
+  if (int(frameCount / 10) % 25 == int(0)) {
+    vshard(random(0, img.width - 100), 100);
+    frameRate(15);
+  } else if (int(frameCount / 10) % 15 == int(0)) {
+    hshard(200, 50);
     frameRate(20);
-  } else if(int(frameCount/2)%40 == int(0)) {
-    mix(random(0, img.width - 150), 150);
-    frameRate(5);
+  } else if (int(frameCount / 2) % 40 == int(0)) {
+    mix(random(0, img.height - 300), 150);
+    frameRate(10);
   } else {
     frameRate(60)
   }
-  
-  
-// movement of red david
-  if (xpos1 == 0) {
-    xspeed1 = - 0.5;
+
+  // draw static overlay
+  if (millis() > mstime + 12000) {
+    static(5, 5);
   }
-  if (xpos1 < -10) {
-    xspeed1 = + 0.5;
+  // stop drawing statick overlay
+  if (millis() > mstime + 14000) {
+    noLoop();
+  } else {
+    loop()
   }
-  xpos1 = xpos1 + xspeed1;
- 
-// movement of blue david
-  if (xpos2 > 10) {
-    xspeed2 = - 0.5;
-  }
-  if (xpos2 == 0) {
-    xspeed2 = + 0.5;
-  }
-  xpos2 = xpos2 + xspeed2;
+}
+
+// mouse press trigger to reset sketch
+
+function mousePressed() {
+  initSetup();
+  initDraw();
 }
 
 
-// rename parameters to y and w
+// vertical shards overlay
 
-function shard (shard1, shard2) {
-  getshard = img.get(0, shard1, img.width, shard2);
-  getshard.filter(POSTERIZE, 3);
-  tint(random(150,247), 52, 122, 255);
-  // tint(random(100, 239), random(0, 187), random(100, 255), 255);
-  image(getshard, 0, shard1, img.width, shard2);
-  // filter(INVERT);
+function vshard(x, y) {
+  getvshard = img.get(x, 0, y, img.height);
+  getvshard.filter(POSTERIZE, 3);
+  tint(random(50, 247), 0, 255, 255);
+  image(getvshard, x, 0, y, img.height);
 }
 
-function fshard (fshard1, fshard2) {
-  getfshard = img.get(0, fshard1, img.width, fshard2);
-  getfshard.filter(INVERT);
-  tint(255, 0, 0, 255);
-  image(getfshard, random(-50, 50), fshard1, img.width, fshard2);
+// horizontal shard overlay (eyes)
+
+function hshard(y, h) {
+  gethshard = img.get(0, y, img.width, h);
+  gethshard.filter(INVERT);
+  image(gethshard, random(-15, 15), y, img.width, h);
 }
 
+// shuffled horizontal overlay
 
-function mix (mix1, mix2) {
-  getmix = img.get(0, mix1, img.width, mix2);
+function mix(y, h) {
+  getmix = img.get(0, y, img.width, h);
   getmix.filter(THRESHOLD);
-  // tint(0, 0, 255, 255);
-  let soff = 150;
-  image(getmix, 0, mix1 + soff, img.width, mix2);
+  tint(random(50, 247), 0, 255, 255);
+  let yoff = 150;
+  image(getmix, 0, y + yoff, img.width, h);
+}
+
+// static overlay
+
+function static(w, h) {
+  for (var x = 0; x < img.width; x += w) {
+    for (var y = 0; y < img.height; y += h) {
+      noStroke();
+      fill(random(1, 255), random(1, 255), random(1, 255), 100);
+      rect(x, y, w, h);
+    }
+  }
 }
